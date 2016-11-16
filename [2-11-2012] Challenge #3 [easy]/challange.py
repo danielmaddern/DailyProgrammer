53e0969ee1517ff809e93dbcbbe5307c039d1832
Daniel Maddern <daniel.maddern@lotterywest.wa.gov.au>
2016-11-16 22:31:37 +0800

    Started challange 3

---
+++ [2-11-2012] Challenge #3 [easy]/challange.py
@@ -0,0 +1,26 @@
+"""
+Welcome to cipher day!
+write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.
+for extra credit, add a "decrypt" function to your program!
+"""
+
+user_choice = raw_input('Would you like to [e]ncrypt or [d]ecrypt? ')
+
+while True:
+    if user_choice.lower() == 'e':
+        shift_by_
+        break
+    elif user_choice.lower() == 'd':
+
+        break
+
+    print "%s is not a valid option not entered, try again." % user_choice
+    user_choice = raw_input('Would you like to [e]ncrypt or [d]ecrypt? ')
+
+
+# output = "your name is %s, you are %s years old, and your username is %s" % (name, age, username)
+# print output
+
+# Extra Credit
+# with open('output.file', 'w') as f:
+#    f.write(output)
