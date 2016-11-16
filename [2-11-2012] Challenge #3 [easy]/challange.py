a383371fc7edde395bc0f36fe3017777d3935178
Daniel Maddern <daniel.maddern@lotterywest.wa.gov.au>
2016-11-16 23:30:13 +0800

    Challange 3 encryption code completed.

--- [2-11-2012] Challenge #3 [easy]/challange.py
+++ [2-11-2012] Challenge #3 [easy]/challange.py
@@ -1,26 +1,42 @@
 """
+https://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/
 Welcome to cipher day!
 write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.
 for extra credit, add a "decrypt" function to your program!
 """
-
-user_choice = raw_input('Would you like to [e]ncrypt or [d]ecrypt? ')
+letters = list('abcdefghijklmnopqrstuvwxyz')
 
 while True:
+    # Find out what the user wants to do
+    user_choice = raw_input('Would you like to [e]ncrypt, [d]ecrypt or [q]uit?  ')
     if user_choice.lower() == 'e':
-        shift_by_
-        break
+        encrypted_text = ''
+        while True:
+            try:
+                shift_by_number_of_letters = int(raw_input('Enter the number of letters you wish to shift by: '))
+                break
+            except ValueError:
+                print "You didn't enter a number, Try again."
+        string_to_encrypt = list(raw_input('Enter the string you would like to encrypt: '))
+
+        for x in string_to_encrypt:
+            added_to_encrypted_string = False
+            for y in letters:
+                if x.lower() == y:
+                    new_letter = shift_by_number_of_letters + letters.index(y)
+                    if new_letter > 25:
+                        new_letter -= 26
+                    encrypted_text = encrypted_text + letters[new_letter]
+                    added_to_encrtyped_string = True
+            if added_to_encrypted_string == False:
+                encrypted_text = encrypted_text + x
+
+        print "You encrypted text is: %s" % encrypted_text
+
     elif user_choice.lower() == 'd':
-
+        pass
+    elif user_choice.lower() == 'q':
         break
-
-    print "%s is not a valid option not entered, try again." % user_choice
-    user_choice = raw_input('Would you like to [e]ncrypt or [d]ecrypt? ')
-
-
-# output = "your name is %s, you are %s years old, and your username is %s" % (name, age, username)
-# print output
-
-# Extra Credit
-# with open('output.file', 'w') as f:
-#    f.write(output)
+    else:
+        print "%s is not a valid option not entered, try again." % user_choice
+        user_choice = raw_input('Would you like to [e]ncrypt, [d]ecrypt or [q]uit?  ')
