7ff7c41e734d14fa09e73378a49ca027a2c33b99
danielmaddern <daniel.maddern@lotterywest.wa.gov.au>
2016-11-18 12:00:49 +0800

    Challange 7 completed

---
+++ [2-15-2012] Challenge #7 [easy]/challange.py
@@ -0,0 +1,46 @@
+"""
+Write a program that can translate Morse code in the format of ...---...
+A space and a slash will be placed between words. ..- / --.-
+For bonus, add the capability of going from a string to Morse code.
+Super-bonus if your program can flash or beep the Morse.
+This is your Morse to translate:
+.... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.--
+"""
+
+morse_code__to_convert = '.... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.--'
+converted_text = ''
+morse_dict = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
+for x in morse_code__to_convert.split('/'):
+    word = x.split()
+    for y in word:
+        converted_text += morse_dict[y]
+    converted_text += ' '
+print converted_text
