3270a8a9a8990cf88dafc48c2c13eb48770431b4
danielmaddern <daniel.maddern@lotterywest.wa.gov.au>
2016-11-23 12:52:39 +0800

    fixed a counting error, changed x to beers.

--- [2-16-2012] Challenge #8 [easy]/challange.py
+++ [2-16-2012] Challenge #8 [easy]/challange.py
@@ -3,10 +3,13 @@ write a program that will print the song "99 bottles of beer on the wall".
 for extra credit, do not allow the program to print each loop on a new line.
 """
 
-for x in range(99, -1, -1):
-    if x == 0:
+for beers in range(99, -1, -1):
+    if beers == 0:
         print 'No more bottles of beer on the wall, no more bottles of beer.'
         print 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    elif beers == 1:
+        print '%s bottles of beer on the wall, %s bottles of beer.' % (beers, beers)
+        print 'Take one down and pass it around, no more bottles of beer on the wall. \n'
     else:
-        print '%s bottles of beer on the wall, %s bottles of beer.' % (x, x)
-        print 'Take one down and pass it around, %s bottles of beer on the wall. \n' % x
+        print '%s bottles of beer on the wall, %s bottles of beer.' % (beers, beers)
+        print 'Take one down and pass it around, %s bottles of beer on the wall. \n' % (beers - 1)
